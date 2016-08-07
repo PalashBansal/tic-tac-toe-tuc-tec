15f07aa270fb8e5b33cace47c8b7ccfa922cc2e7
Palash Bansal <palash.bansal20@gmail.com>
2016-08-08 01:32:25 +0530

    commit everything

--- tic tac toe tuc tec/tic tac toe tuc tec/main.cpp
+++ tic tac toe tuc tec/tic tac toe tuc tec/main.cpp
@@ -8,40 +8,85 @@
 
 #include<iostream>
 #include<string>
+#include<conio.h>
+
+#include<SDL/SDL.h>
+#include<GL/glew.h>
 
 #include "graphics/GameWindow.hpp"
 
 using namespace std;
 
-void error_msg(string errorMsg);
+enum GameState;
+
+void console_msg(string);
 int abort_main_app();
+void GameLoop();
+GameState ProcessInput();
 
 int main(int argc, char** argv)
 {
-	GameWindow gm;
-	bool _iswindowCreated = gm.initSystems();
+	GameWindow gw;
+	bool _iswindowCreated = gw.CreateGameWindow();
 	if(!_iswindowCreated)
 	{
-		error_msg("Main Game Window not able to create!");
-		return abort_main_app();
+		console_msg("error: Main Game Window not able to create!");
+		int abort_result=-1;
+		//abort_result = abort_main_app();	// only used in Debug mode
+		return abort_result;
 	}
 	else
 	{
-		char cHoldScreen;
-		cin >> cHoldScreen;
+		console_msg("Main Game Window created!");
+		//
+		GameLoop();
+		//
 	}
 	return 0;
 }
 
-void error_msg(string errorMsg)
+enum GameState
 {
-	cout << errorMsg << endl;
+	PLAY,
+	EXIT
+};
+
+void GameLoop()
+{
+	GameState gameState=PLAY;
+	while(gameState!=EXIT)
+	{
+		gameState = ProcessInput();
+	}
+}
+
+GameState ProcessInput()
+{
+	GameState temp_gameState=PLAY;
+	SDL_Event sdlEvent;	// takes input from window
+	while(SDL_PollEvent(&sdlEvent))	// return from SDL_PollEvent is: 1 is true and 0 is false
+	{
+		switch(sdlEvent.type)
+		{
+		case SDL_QUIT:
+			temp_gameState = EXIT;
+			break;
+		case SDL_MOUSEMOTION:
+			cout << sdlEvent.motion.x << ", " << sdlEvent.motion.y << endl;
+			break;
+		}
+	}
+	return temp_gameState;
+}
+
+void console_msg(string msg)
+{
+	cout << msg << endl;
 }
 
 int abort_main_app()
 {
-	int dumpIntInput;
 	cout << "Press any key to quit app..." << endl;
-	cin >> dumpIntInput;
+	(void)getch();
 	return -1;
 }
