a0edc8698cf64d7f63ce7c81d984da3c563c6989
Palash Bansal <palash.bansal20@gmail.com>
2016-08-07 19:30:41 +0530

    setting SDL
    setting up SDL and main.cpp.
    deps/SDL is downloaded SDL.
    all .lib & .dll are taken from x86(Win32) version.
    disabled Common Language Runtime Support(/CLR).

---
+++ tic tac toe tuc tec/tic tac toe tuc tec/main.cpp
@@ -0,0 +1,16 @@
+/*******************************************************************************
+* Project tic tac toe tuc tec
+*******************************************************************************/
+/**
+* @file main.cpp
+* @author BPalash
+*/
+
+#include <SDL\SDL.h>
+
+int main(int argc, char** argv)
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+
+	return 0;
+}
